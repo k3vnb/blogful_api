e938f9472e5af568c076f59b2962a188d740587c
Kevin Boyle <papershack@gmail.com>
2019-09-14 14:49:27 -0700

    add users-router and users-service files, import these into app.js

---
+++ src/users/users-service.js
@@ -0,0 +1,31 @@
+const UsersService = {
+    getAllUsers(knex){
+        return knex.select('*').from('blogful_users')
+    },
+    insertUser(knex, newUser){
+        return knex
+            .insert(newUser)
+            .into('blogful_users')
+            .return('*')
+            .then(rows => rows[0])
+    },
+    getById(knex, id){
+        return knex
+            .select('*')
+            .from('blogful_users')
+            .where('id', id)
+            .first()
+    },
+    deleteUser(knex, id){
+        return knex('blogful_users')
+            .where({ id })
+            .delete()
+    },
+    updateUser(knex, id, newUserFields){
+        return knex('blogful_user')
+            .where({ id })
+            .update(newUserFields)
+    }
+}
+
+module.exports = UsersService
