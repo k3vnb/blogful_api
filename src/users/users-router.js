29250a323bf4577e54c4ccf12a33fcf51859b28a
Kevin Boyle <papershack@gmail.com>
2019-09-14 18:11:12 -0700

    add new comments-router.js & comments-service.js files to accomodate comments in blogful & db, import these into app.js... test in Postman shows correct configuration

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,106 +1,95 @@
-const path = require('path')
-const express = require('express')
-const xss = require('xss')
-const UsersService = require('./users-service')
+const path = require("path");
+const express = require("express");
+const xss = require("xss");
+const UsersService = require("./users-service");
 
-const usersRouter = express.Router()
-const jsonParser = express.json()
+const usersRouter = express.Router();
+const jsonParser = express.json();
 
 const serializeUser = user => ({
-    id: user.id,
-    fullname: xss(user.fullname),
-    username: xss(user.username),
-    nickname: xss(user.nickname),
-    date_created: user.date_created,
-})
+  id: user.id,
+  fullname: xss(user.fullname),
+  username: xss(user.username),
+  nickname: xss(user.nickname),
+  date_created: user.date_created
+});
 
 usersRouter
-    .route('/')
-    .get((req, res, next) => {
-        const knexInstance = req.app.get('db')
-        UsersService.getAllUsers(knexInstance)
-            .then(users => {
-                res.json(users.map(serializeUser))
-            })
-            .catch(next)
-    })
-    .post(jsonParser, (req, res, next) => {
-      const { fullname, username, nickname, password } = req.body
-      const newUser = { fullname, username }
-
-      for (const [key, value] of Object.entries(newUser)){
-          if (value == null){
-              return res.status(400).json({
-                  error: { message: `Missing '${key}' in request body` }
-              })
-          }
-      }
-
-      newUser.nickname = nickname
-      newUser.password = password
-
-      UsersService.insertUser(
-          req.app.get('db'),
-          newUser
-      )
-      .then(user => {
-          res
-            .status(201)
-            .location(path.posix.join(req.originalUrl, `/${user.id}`))
-            .json(serializeUser(user))
+  .route("/")
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    UsersService.getAllUsers(knexInstance)
+      .then(users => {
+        res.json(users.map(serializeUser));
       })
-      .catch(next)
-    })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { fullname, username, nickname, password } = req.body;
+    const newUser = { fullname, username };
 
-    usersRouter
-        .route('/:user_id')
-        .all((req, res, next) => {
-          UsersService.getById(
-              req.app.get('db'),
-              req.params.user_id
-          )
-          .then(user => {
-              if (!user){
-                  res.status(404).json({
-                      error: { message: `User doesn't exist` }
-                  })
-              }
-              res.user = user
-              next()
-          })
-          .catch(next)
-        })
-        .get((req, res, next) => {
-          res.json(serializeUser(res.user))
-        })
-        .delete((req, res, next) => {
-          UsersService.deleteUser(
-              req.app.get('db'),
-              req.params.user_id
-          )
-            .then(numRowsAffected => {
-                res.status(204).end()
-            })
-            .catch(next)
-        })
-        .patch(jsonParser, (req, res, next) => {
-          const { fullname, username, password, nickname } = req.body 
-          const userToUpdate = { fullname, username, password, nickname }
+    for (const [key, value] of Object.entries(newUser)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        });
+      }
+    }
 
-          const numberOfValues = Object.values(userToUpdate).filter(Boolean).length
-          if (numberOfValues === 0) 
-            return res.status(400).json({  
-                error: { message: `Request must contain either 'fullname', 'username', 'password', or 'nickname'` } 
-            })
-            UsersService.updateUser(
-                req.app.get('db'),
-                req.params.user_id,
-                userToUpdate
-            )
-            .then(numRowsAffected => {
-                res.status(204).end()
-            })
-            .catch(next)
-        })
+    newUser.nickname = nickname;
+    newUser.password = password;
 
-module.exports = usersRouter
+    UsersService.insertUser(req.app.get("db"), newUser)
+      .then(user => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${user.id}`))
+          .json(serializeUser(user));
+      })
+      .catch(next);
+  });
+
+usersRouter
+  .route("/:user_id")
+  .all((req, res, next) => {
+    UsersService.getById(req.app.get("db"), req.params.user_id)
+      .then(user => {
+        if (!user) {
+          res.status(404).json({
+            error: { message: `User doesn't exist` }
+          });
+        }
+        res.user = user;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeUser(res.user));
+  })
+  .delete((req, res, next) => {
+    UsersService.deleteUser(req.app.get("db"), req.params.user_id)
+      .then(numRowsAffected => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { fullname, username, password, nickname } = req.body;
+    const userToUpdate = { fullname, username, password, nickname };
+
+    const numberOfValues = Object.values(userToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request must contain either 'fullname', 'username', 'password', or 'nickname'`
+        }
+      });
+    UsersService.updateUser(req.app.get("db"), req.params.user_id, userToUpdate)
+      .then(numRowsAffected => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = usersRouter;
